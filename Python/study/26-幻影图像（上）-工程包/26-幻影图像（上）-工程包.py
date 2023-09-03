16ccd833ff7fd38c50880bb96ff2a5d8277036e5
XueJourney <143483602+XueJourney@users.noreply.github.com>
2023-09-03 10:37:28 +0800

    提交26-幻影图像（上）代码

--- Python/study/26-幻影图像（上）-工程包/26-幻影图像（上）-工程包.py
+++ Python/study/26-幻影图像（上）-工程包/26-幻影图像（上）-工程包.py
@@ -6,9 +6,41 @@ def main(img1, img2):
     img2_ = Image.open(img2).convert("L")
     # 将img_图片调整为img_的尺寸
     img1_ = img1_.resize(img2_.size)
-    img1_.show()
-    img2_.show()
+    # img1_.show()
+    # img2_.show()
+    # 使用point()方法,优化两张图片
+    img1_ = img1_.point(changeL)
+    img2_ = img2_.point(changeD)
+    # 调用first函数,实现照片合成
+    first(img1_, img2_).save("./img/result.jpg")
+
+# 定义函数changeL,将像素值变为原先的1.1倍
+def changeL(img):
+    return img * 1.1
+
+# 定义函数changeD,将像素值变为原先的1.1倍
+def changeD(img):
+    return img * 1.1
+
+# 定义函数first,实现照片合成,传入两个参数--imga和imgb,分别代表两张照片
+def first(imga, imgb):
+    img1 = imga.load()
+    img2 = imgb.load()
+    # 获取图片的宽和高
+    width, height = imga.size
+    # 创建一个新图片
+    for i in range(width):
+        for j in range(height):
+            a = img1[i, j]
+            b = img2[i, j]
+            # 公式进行合成每个像素点
+            color = 255-a+b
+            if color > 200:
+                img1[i,j] = 160 - a + b
+            else:
+                img1[i,j] = color
+    return imga
 
 # 调用main函数
 if "__main__" == __name__:
-    main("Python/study/26-幻影图像（上）-工程包//1.jpg", "Python/study/26-幻影图像（上）-工程包/img/2.jpg")
+    main("./img/1.png", "./img/2.png")
