861097aba73a5bf719215179fa9dd9731f067880
Mr Xue <2432287445@qq.com>
2023-09-24 09:33:32 +0800

    添加文字，添加动画和计算

--- HTML/xuezhongze.xinghuo.website/js/script.js
+++ HTML/xuezhongze.xinghuo.website/js/script.js
@@ -34,3 +34,17 @@ textLines.forEach((line) => {
     line.style.animationDelay = `${delay}ms`;
     delay += animationDuration + delayBetweenLines;
 });
+
+
+// 2020距离今年的时间
+// 获取当前年份
+const currentYear = new Date().getFullYear();
+
+// 计算年份差值
+const yearDifference = currentYear - 2020;
+
+// 获取显示年份差值的元素
+const yearDifferenceElement = document.getElementById('year-difference');
+
+// 更新年份差值的文本内容
+yearDifferenceElement.textContent = yearDifference;
