f33421fa840a1f665ff70b36c11ca721b18c381e
MrXue <admin@xinghuo.website>
2023-10-14 18:10:48 +0800

    更新QSP速率限制

--- HTML/www.xn--6qq986b3xl.world/app.js
+++ HTML/www.xn--6qq986b3xl.world/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 const port = 7001;
 
-// 设置静态目录
+// 从 'public' 目录提供静态文件
 app.use(express.static('public'));
 
-// 调用函数读取页面文件
+// 定义一个函数，用于从文件中读取页面内容
 function readPage(filePath, variableName) {
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, 'utf8', (err, data) => {
@@ -15,7 +16,6 @@ function readPage(filePath, variableName) {
                 console.error('读取文件出错:', err);
                 reject(err);
             } else {
-                // 将文件内容赋值给指定变量名
                 global[variableName] = data;
                 resolve();
             }
@@ -23,65 +23,72 @@ function readPage(filePath, variableName) {
     });
 }
 
-// 中间件函数来获取用户IP、请求方式和请求头信息并记录日志
+// 中间件，用于记录请求信息
 function logRequestInfo(req, res, next) {
-    // 获取当前时间戳
     const timestamp = new Date().toISOString();
-
-    console.log('Request URL:', req.url);
     const userIP = req.socket.remoteAddress;
     const requestMethod = req.method;
     const requestHeaders = JSON.stringify(req.headers, null, 2);
 
-    // 格式化日志信息
-    const logMessage = `${timestamp}\nRequest URL: ${req.url}\nUser IP: ${userIP}\nRequest Method: ${requestMethod}\nRequest Headers: ${requestHeaders}\n\n`;
+    const logMessage = `${timestamp}\n请求 URL: ${req.url}\n用户 IP: ${userIP}\n请求方法: ${requestMethod}\n请求头信息: ${requestHeaders}\n\n`;
 
-    // 输出到控制台
     console.log(logMessage);
 
-    // 保存日志到文件
     const now = new Date();
     const year = now.getFullYear();
     const month = (now.getMonth() + 1).toString().padStart(2, '0');
     const day = now.getDate().toString().padStart(2, '0');
     const logFileName = `${year}${month}${day}.log`;
 
-    const logPath = './log/' + logFileName;
+    const logPath = `./log/${logFileName}`;
     fs.appendFile(logPath, logMessage, (err) => {
         if (err) {
-            console.error('Error writing to log file:', err);
+            console.error('写入日志文件出错:', err);
         }
     });
 
     next();
 }
 
-// 在这里使用 Promise 来确保页面文件加载完成
+readPage('./error/429.html', 'error_429')
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 60,
+    keyGenerator: (req) => req.ip,
+    handler: (req, res) => {
+        // Send a 429 response with the custom message
+        res.status(429).send(error_429);
+    },
+});
+
+// 使用 Promise 来确保页面文件加载完成
 Promise.all([
     readPage('./error/404.html', 'error_404'),
     readPage('./HTML/index.html', 'index'),
     readPage('./HTML/ownership.html', 'ownership')
 ])
 .then(() => {
-    // 使用中间件来获取请求信息
     app.use(logRequestInfo);
+    app.use(limiter);
 
     // 设置路由
-    app.get(['/', '/index','/index.html'], (req, res) => {
-        // 指定响应内容的类型和编码
+    app.get(['/', '/index', '/index.html'], (req, res) => {
         res.set('Content-Type', 'text/html; charset=utf-8');
         res.send(index);
     });
 
-    app.get(['/ownership','/ownership.html'], (req, res) => {
-        // 指定响应内容的类型和编码
+    app.get(['/ownership', '/ownership.html'], (req, res) => {
         res.set('Content-Type', 'text/html; charset=utf-8');
         res.send(ownership);
     });
 
-    // 设置404页面
     app.use((req, res, next) => {
-        res.status(404).send(error_404);
+        const status = res.statusCode;
+        if (status === 404) {
+            res.send(error_404);
+        } else {
+            next();
+        }
     });
 
     // 创建服务器并监听端口
