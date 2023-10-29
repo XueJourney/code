8da14af0f292e745749edeceedc42cc4fbee95f1
Mr Xue <2432287445@qq.com>
2023-10-29 10:52:22 +0800

    格式化输入课程

---
+++ C++/study/Format_input/20231029_1.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    char a, b, c, d, e;
+    scanf("%c %c %c %c %c", &a, &b, &c, &d, &e);
+    printf("    %c\n   *%c*\n  **%c**\n ***%c***\n****%c****\n", a, b, c, d, e);
+    system("pause");
+    return 0;
+}
---
+++ C++/study/Format_input/20231029_2.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+# include <cmath>
+
+using namespace std;
+int main() {
+    float n = 3.1415926;
+    printf("%f", round(n));
+    system("pause");
+    return 0;
+}
