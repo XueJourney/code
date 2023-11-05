1d39449da8e51a7f083e9156ebcbceb5b8c94d09
Mr Xue <2432287445@qq.com>
2023-11-05 09:32:04 +0800

    修复代码

--- C++/OJ/codemao/1013.cpp
+++ C++/OJ/codemao/1013.cpp
@@ -5,18 +5,17 @@ using namespace std;
 int main() {
     // 声明变量来存储高度和半径
     int h, r;
-
     // 输入高度和半径
     cin >> h >> r;
-
-    // 计算每个小圆桶的容积
-    double bucket_volume = 3.14159 * pow(r, 2) * h;
-
-    // 计算所需的桶数，并向上取整
-    int buckets_needed = ceil(20.0 / bucket_volume);
-
-    // 输出结果
-    cout << buckets_needed << endl;
+    // 圆周率
+    float pi;
+    pi = 3.14159;
+    // 计算水的体积
+    float volume;
+    volume = pi * r * r * h;
+    int res;
+    res = ceil(20 / (volume/1000));
+    cout << res;
 
     return 0;
 }
