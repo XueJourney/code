ff10b47ab11ea2db98287e7cb0a18d13b0d3985f
MrXue <2432287445@qq.com>
2023-12-09 23:16:04 +0800

    aa1刷新

--- Python/API/idcard/post.py
+++ Python/API/idcard/post.py
@@ -1,21 +1,22 @@
 import requests
+import tqdm
 
-def authenticate(idcard, name):
-    url = "http://api.xinghuo.website/api/idcard"
-    params = {
-        "idcard": idcard,
-        "name": name
-    }
-    response = requests.get(url, params=params)
-    # response2 = requests.post(url, data=params)
-    # result = response.json()
-    return [response,response.text]
+# def authenticate(idcard, name):
+#     url = "http://127.0.0.1:7005/api/idcard"
+#     params = {
+#         "idcard": idcard,
+#         "name": name
+#     }
+#     response = requests.get(url, params=params)
+#     # response2 = requests.post(url, data=params)
+#     # result = response.json()
+#     return [response,response.text]
 
-idcard = "123456789101"
-name = "张三"
-# print(authenticate(idcard, name))
+# idcard = "123456789101"
+# name = "张三"
+# # print(authenticate(idcard, name))
 result = []
-for i in range(1):
-    result.append(authenticate(idcard, name))
+for i in tqdm.tqdm(range(50)):
+    result.append(requests.get("https://api.aa1.cn/doc/authenticate.html"))
 # result2.append(authenticate(idcard, name))
 print(result)
