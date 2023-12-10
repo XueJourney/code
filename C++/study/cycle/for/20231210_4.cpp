c47f84b59ce634976e6c2687c85197289ce9c3dc
Mr Xue <2432287445@qq.com>
2023-12-10 10:20:10 +0800

    等差数列

---
+++ C++/study/cycle/for/20231210_4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int f,i,j;
+    // scanf("%d %d %d",&f,&i,&j);
+    f = 3;
+    i = 4;
+    j = 3;
+    for(int k=0;k<=i;k++){
+        cout<<f+k*j<<" ";
+    }
+}
