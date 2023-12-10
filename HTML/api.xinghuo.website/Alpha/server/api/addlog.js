982774f6e4aa351526fc07da018e656626ae0448
Mr Xue <2432287445@qq.com>
2023-12-10 09:43:36 +0800

    修复一直保存到程序启动日期文件的bug

--- HTML/api.xinghuo.website/Alpha/server/api/addlog.js
+++ HTML/api.xinghuo.website/Alpha/server/api/addlog.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
 const winston = require('winston');
+require('winston-daily-rotate-file');
 
 // 确保总目录存在
 const logDirectory = './logs/general';
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
-// 创建一个新的带有文件传输功能的 winston 日志记录器实例
+// 创建一个新的带有每日轮替文件传输功能的 Winston 日志记录器实例
+const dailyRotateFileTransport = new winston.transports.DailyRotateFile({
+    filename: `${logDirectory}/%DATE%.log`,
+    datePattern: 'YYYY-MM-DD'
+});
+
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports: [
-        new winston.transports.File({ filename: `${logDirectory}/${new Date().toISOString().slice(0,10)}.log` })
+        dailyRotateFileTransport
     ]
 });
 
-function setuplogRoutes(app){
+function setuplogRoutes(app) {
     // 记录所有请求的中间件
     app.use((req, res, next) => {
         const logEntry = {
