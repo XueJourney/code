2a948948e45bc42a821be1669e3e5a9aee660652
Mr Xue <2432287445@qq.com>
2023-12-10 09:15:07 +0800

    n的阶乘

--- C++/study/cycle/20231203_7.cpp
+++ C++/study/cycle/20231203_7.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// 函数声明
-unsigned long long factorial(unsigned int n);
-
 int main() {
-    unsigned int n;
-    cout << "Enter a positive integer: ";
-    cin >> n;
-
-    cout << n << "! = " << factorial(n) << endl;
-    return 0;
-}
-
-// 函数定义
-unsigned long long factorial(unsigned int n) {
-    if (n == 0) return 1; // 0的阶乘是1
-    return n * factorial(n - 1); // 递归计算阶乘
+    int n = 6;
+    int result = 1;
+    for(int i=1; i<n-1 ;i++){
+        result = result * (i+1);
+    }
+    cout << result << endl;
 }
