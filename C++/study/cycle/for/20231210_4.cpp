8e435f9420e3966fe6d958484a962129a6a9a7d4
Mr Xue <2432287445@qq.com>
2023-12-10 10:22:36 +0800

    自定义输入，修复一些bug

--- C++/study/cycle/for/20231210_4.cpp
+++ C++/study/cycle/for/20231210_4.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main(){
     int f,i,j;
-    // scanf("%d %d %d",&f,&i,&j);
-    f = 3;
-    i = 4;
-    j = 3;
-    for(int k=0;k<=i;k++){
+    scanf("%d %d %d",&f,&i,&j);
+    // f = 3;
+    // i = 4;
+    // j = 3;
+    for(int k=0;k<i;k++){
         cout<<f+k*j<<" ";
     }
 }
