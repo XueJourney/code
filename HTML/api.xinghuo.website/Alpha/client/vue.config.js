562e14709e43fb43952bbd8328b38d72e3a13931
MrXue <2432287445@qq.com>
2023-12-10 18:55:16 +0800

    上线

--- HTML/api.xinghuo.website/Alpha/client/vue.config.js
+++ HTML/api.xinghuo.website/Alpha/client/vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    port: 7006,
+    host: 'doc.api.xinghuo.website'
+  },
   publicPath: './',
   transpileDependencies: true
 })
