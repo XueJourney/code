93a35f363c81107bfa2db3d83cd657278df1f9af
Mr Xue <2432287445@qq.com>
2023-12-10 09:55:26 +0800

    修复一直保存到程序启动日期文件的bug

--- C++/study/cycle/for/20231210_2.cpp
+++ C++/study/cycle/for/20231210_2.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main(){
     int i = 0;
+    cout << "i++" << endl;
     cout << i++ << endl;
+    cout << i << endl;
+    cout <<"++i"<<endl;
+    i = 0;
+    cout << ++i << endl;
     cout << i << endl;
 }
